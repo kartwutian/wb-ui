c585f5edfad16d458282a61eb2be723da1ac4e03
wangy <wangyang@zjbdos.com>
2019-11-06 14:44:09 +0800

    tag标记

--- components/vant/tag/index.vue
+++ components/vant/tag/index.vue
@@ -1,19 +1,34 @@
 <template>
+  <!-- <view
+    :class="classes"
+    :style="styles"
+  >
+    <slot />
+  </view> -->
+
   <view
     :class="classes"
     :style="styles"
   >
     <slot />
+    <van-icon
+      v-if="closeable"
+      name="cross"
+      custom-class="van-tag__close"
+      @click="onClose"
+    />
   </view>
 
 </template>
 
 <script>
 import utils from '../wxs/utils'
-import {basic} from "../mixins/basic";
+import { basic } from "../mixins/basic";
+import VanIcon from "../icon/index"
 
 export default {
   name: 'van-tag',
+  components: { VanIcon },
   mixins: [basic],
   props: {
     size: {
@@ -37,21 +52,28 @@ export default {
     type: {
       type: String,
       default: 'default'
+    },
+    closeable: {
+      type: Boolean
     }
   },
   computed: {
-    classes(){
-      return `${this.customClass} ${utils.bem('tag', [this.type, this.size, {mark: this.mark, plain: this.plain, round: this.round }])} ${this.plain ? 'van-hairline--surround' : ''}`
+    classes () {
+      return `${this.customClass} ${utils.bem('tag', [this.type, this.size, { mark: this.mark, plain: this.plain, round: this.round }])} ${this.plain ? 'van-hairline--surround' : ''}`
     },
-    styles(){
+    styles () {
       return `${this.color && !this.plain ? 'background-color: ' + this.color + ';' : ''} ${this.textColor || (this.color && this.plain) ? 'color: ' + (this.textColor || this.color) : ''}`
     }
   },
+  methods: {
+    onClose () {
+      this.$emit("close")
+    }
+  }
 }
 
 
 </script>
 
 <style lang="less">
-
 </style>
