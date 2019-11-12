55ca941835680c6c93f61bfc8664f54cb9bd83c5
bhz <baohz@zjbdos.com>
2019-11-12 10:02:05 +0800

    add divider pages

---
+++ pages/vant/divider/divider.vue
@@ -0,0 +1,24 @@
+<template>
+  <view class="pages-divider">
+
+  </view>
+</template>
+
+<script>
+  export default {
+    name: 'pages-divider',
+    onLoad(){
+
+    },
+    onUnload(){
+
+    },
+    methods: {
+
+    }
+  }
+</script>
+
+<style lang="less">
+
+</style>
