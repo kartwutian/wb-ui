d77a7dff6647137b2ce30793962227d3c94f38a6
wangy <wangyang@zjbdos.com>
2019-11-12 09:30:51 +0800

    panel 面板

--- components/vant/panel/index.vue
+++ components/vant/panel/index.vue
@@ -1,42 +1,58 @@
 <template>
-    <view class="van-panel van-hairline--top-bottom custom-class">
-  <van-cell
-    v-if=" title || desc || status "
-    :title=" title "
-    :label=" desc "
-    :value=" status "
-    custom-class="header-class"
-    value-class="van-panel__header-value"
-  />
-  <slot v-else name="header" />
+  <view class="van-panel van-hairline--top-bottom custom-class">
+    <van-cell
+      v-if=" title || desc || status "
+      :title=" title "
+      :label=" desc "
+      :value=" status "
+      :custom-class="headerClass"
+      value-class="van-panel__header-value"
+    />
+    <slot
+      v-else
+      name="header"
+    />
 
-  <view class="van-panel__content">
-    <slot />
-  </view>
+    <view class="van-panel__content">
+      <slot />
+    </view>
 
-  <view v-if=" useFooterSlot " class="van-panel__footer van-hairline--top footer-class">
-    <slot name="footer" />
+    <view
+      v-if=" useFooterSlot "
+      :class="'van-panel__footer van-hairline--top ' + footerClass"
+    >
+      <slot name="footer" />
+    </view>
   </view>
-</view>
 
 </template>
 
 <script>
+import VanCell from '../cell/index'
 
 
 export default {
-  classes: ['header-class', 'footer-class'],
+  name: "van-panel",
+  components: { VanCell },
+  // classes: ['header-class', 'footer-class'],
 
   props: {
     desc: String,
     title: String,
     status: String,
-    useFooterSlot: Boolean
+    useFooterSlot: Boolean,
+    headerClass: {
+      type: String,
+      default: ""
+    },
+    footerClass: {
+      type: String,
+      default: ""
+    }
   }
 };
 
 </script>
 
 <style lang="less">
-
 </style>
--- pages/vant/panel/panel.vue
+++ pages/vant/panel/panel.vue
@@ -1,26 +1,69 @@
 <template>
   <view class="pages-panel">
 
+    <demo-block title="基础用法">
+      <van-panel
+        title="标题"
+        desc="描述信息"
+        status="状态"
+      >
+        <view class="content">内容</view>
+      </van-panel>
+    </demo-block>
+
+    <demo-block title="高级用法">
+      <van-panel
+        title="标题"
+        desc="描述信息"
+        status="状态"
+        use-footer-slot
+      >
+        <view class="content">内容</view>
+        <view
+          slot="footer"
+          class="footer"
+        >
+          <van-button
+            size="small"
+            class="demo-margin-right"
+          >按钮</van-button>
+          <van-button
+            size="small"
+            type="danger"
+          >按钮</van-button>
+        </view>
+      </van-panel>
+    </demo-block>
   </view>
 </template>
 
 <script>
-  import DemoBlock from "../../../components/app/demo-block";
+import DemoBlock from "@/components/app/demo-block";
+import VanButton from "@/components/vant/button/index";
+import VanPanel from '@/components/vant/panel/index';
 
-  export default {
-    name: 'pages-panel',
-    onLoad(){
+export default {
+  name: 'pages-panel',
+  components: { VanPanel, VanButton, DemoBlock },
+  onLoad () {
 
-    },
-    onUnload(){
+  },
+  onUnload () {
 
-    },
-    methods: {
+  },
+  methods: {
 
-    }
   }
+}
 </script>
 
 <style lang="less">
+.content {
+  padding: 20px;
+  font-size: 16px;
+}
 
+.footer {
+  text-align: right;
+}
 </style>
