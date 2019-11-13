68fa2be632739635f27ac9ec635a9677c019c749
bhz <baohz@zjbdos.com>
2019-11-13 09:32:14 +0800

    复选框完成，relation迁移方案可参考此实现，基本通用

--- components/vant/checkbox/index.vue
+++ components/vant/checkbox/index.vue
@@ -1,125 +1,176 @@
 <template>
 
-<view class="van-checkbox custom-class">
-  <view class="van-checkbox__icon-wrap" bindtap="toggle">
-    <slot v-if=" useIconSlot " name="icon" />
-    <van-icon
-      v-else
-      name="success"
-      size="0.8em"
-      class="{{ utils.bem('checkbox__icon', [shape, { disabled, checked: value }]) }}"
-      style="font-size: {{ sizeWithUnit }};{{ checkedColor && value && !disabled ? 'border-color:' + checkedColor + '; background-color:' + checkedColor : '' }}"
-      custom-class="icon-class"
-      custom-style="line-height: 1.25em;"
-    />
+  <view :class="'van-checkbox ' + customClass">
+    <view class="van-checkbox__icon-wrap" @tap="toggle">
+      <slot v-if=" useIconSlot " name="icon"/>
+      <van-icon
+          v-else
+          name="success"
+          size="0.8em"
+          :class="iconClasses"
+          :style="iconStyle"
+          :custom-class="iconClass"
+          custom-style="line-height: 1.25em;"
+      />
+    </view>
+    <view :class="labelClasses" @tap="onClickLabel">
+      <slot/>
+    </view>
   </view>
-  <view class="label-class {{ utils.bem('checkbox__label', [labelPosition, { disabled }]) }}" bindtap="onClickLabel">
-    <slot />
-  </view>
-</view>
-
 </template>
 
 <script>
   import utils from '../wxs/utils';
+  import {basic} from "../mixins/basic";
+  import {addUnit, queryParentComponent} from '../common/utils';
+  import VanIcon from "../icon/index";
 
-import { addUnit } from '../common/utils';
-
-function emit(target: WechatMiniprogram.Component.TrivialInstance, value: boolean | any[]) {
-  target.$emit('input', value);
-  target.$emit('change', value);
-}
-
-export default {
-  field: true,
-
-  relation: {
-    name: 'checkbox-group',
-    type: 'ancestor',
-    linked(target) {
-      this.parent = target;
-    },
-    unlinked() {
-      this.parent = null;
-    }
-  },
-
-  classes: ['icon-class', 'label-class'],
-
-  props: {
-    value: Boolean,
-    disabled: Boolean,
-    useIconSlot: Boolean,
-    checkedColor: String,
-    labelPosition: String,
-    labelDisabled: Boolean,
-    shape: {
-      type: String,
-      value: 'round'
-    },
-    iconSize: {
-      type: null,
-      observer: 'setSizeWithUnit'
-    }
-  },
-
-  data: {
-    sizeWithUnit: '20px'
-  },
-
-  methods: {
-    emitChange(value: boolean) {
-      if (this.parent) {
-        this.setParentValue(this.parent, value);
-      } else {
-        emit(this, value);
-      }
-    },
-
-    toggle() {
-      const { disabled, value } = this.data;
-      if (!disabled) {
-        this.emitChange(!value);
-      }
-    },
-
-    onClickLabel() {
-      const { labelDisabled, disabled, value } = this.data;
-      if (!disabled && !labelDisabled) {
-        this.emitChange(!value);
-      }
-    },
-
-    setParentValue(parent: WechatMiniprogram.Component.TrivialInstance, value: boolean) {
-      const parentValue = parent.data.value.slice();
-      const { name } = this.data;
-      const { max } = parent.data;
-
-      if (value) {
-        if (max && parentValue.length >= max) {
-          return;
-        }
-
-        if (parentValue.indexOf(name) === -1) {
-          parentValue.push(name);
-          emit(parent, parentValue);
-        }
-      } else {
-        const index = parentValue.indexOf(name);
-        if (index !== -1) {
-          parentValue.splice(index, 1);
-          emit(parent, parentValue);
-        }
-      }
-    },
-
-    setSizeWithUnit(size: string | number): void {
-      this.set({
-        sizeWithUnit: addUnit(size)
-      });
-    },
+  function emit(target, value) {
+    target.$emit('input', value);
+    target.$emit('change', value);
   }
-};
+
+  export default {
+    name: 'van-checkbox',
+    components: {VanIcon},
+    mixins: [basic],
+    // classes: ['icon-class', 'label-class'],
+    props: {
+      name: {
+        type: String,
+      },
+      value: {
+        type: Boolean,
+        default: false,
+      },
+      disabled: Boolean,
+      useIconSlot: Boolean,
+      checkedColor: {
+        type: String,
+      },
+      labelPosition: String,
+      labelDisabled: Boolean,
+      shape: {
+        type: String,
+        default: 'round'
+      },
+      iconClass: {
+        type: String,
+        default: ''
+      },
+      labelClass: {
+        type: String,
+        default: ''
+      },
+      iconSize: {
+        type: null,
+      },
+    },
+
+
+    computed: {
+      iconClasses() {
+        return `${utils.bem('checkbox__icon', [this.shape, {disabled: this.isDisabled, checked: this.checked}])}`
+      },
+      iconStyle() {
+        return `font-size: ${this.sizeWithUnit};${this.checkedColor && this.checked && !this.isDisabled ? 'border-color:' + this.checkedColor + '; background-color:' + this.checkedColor : ''}`
+      },
+      labelClasses() {
+        return `${this.labelClass} ${utils.bem('checkbox__label', [this.labelPosition, {disabled: this.isDisabled}])}`
+      },
+      sizeWithUnit() {
+        return addUnit(this.iconSize)
+      },
+      checked: {
+        get() {
+          return this.hasParent ? this.proxyValue : this.value;
+        },
+      },
+      isDisabled: {
+        get() {
+          return this.hasParent ? this.disabled || this.proxyDisabled: this.disabled;
+        },
+      },
+    },
+
+    data(){
+      return {
+        hasParent: false,
+        proxyValue: false,
+        proxyDisabled: false,
+      }
+    },
+
+    mounted() {
+      this.parent = queryParentComponent(this, 'van-checkbox-group');
+      if(this.parent){
+        this.hasParent = true;
+        this.parent.linked(this);
+      }
+    },
+
+    destroyed() {
+      if(this.parent){
+        this.parent.unlinked(this);
+        this.parent = null;
+        this.hasParent = false;
+      }
+    },
+
+    methods: {
+
+      emitChange(value) {
+        if(this.parent){
+          this.setParentValue(this.parent, value);
+        }
+        emit(this, value);
+      },
+
+      toggle() {
+        const {disabled, value, proxyValue, proxyDisabled, hasParent} = this;
+        if (!proxyDisabled && !disabled) {
+          this.emitChange(hasParent ? !proxyValue: !value);
+
+          // When toggle method is called multiple times at the same time,
+          // only the last call is valid.
+          // This is a hack for usage inside Cell.
+          // clearTimeout(this.toggleTask);
+          // this.toggleTask = setTimeout(() => {
+          //   this.checked = !value;
+          // });
+        }
+      },
+
+      onClickLabel() {
+        const {labelDisabled, disabled, value, proxyValue, proxyDisabled, hasParent} = this;
+        if (!proxyDisabled && !disabled && !labelDisabled) {
+          this.emitChange(hasParent ? !proxyValue: !value);
+        }
+      },
+      setParentValue(parent, value) {
+        const parentValue = parent.value.slice();
+        const { name } = this;
+        const { max } = parent;
+        if (value) {
+          if (max && parentValue.length >= max) {
+            return;
+          }
+
+          if (parentValue.indexOf(name) === -1) {
+            parentValue.push(name);
+            emit(parent, parentValue);
+          }
+        } else {
+          const index = parentValue.indexOf(name);
+          if (index !== -1) {
+            parentValue.splice(index, 1);
+            emit(parent, parentValue);
+          }
+        }
+      },
+    },
+
+  };
 
 </script>
 
--- pages/vant/checkbox/checkbox.vue
+++ pages/vant/checkbox/checkbox.vue
@@ -1,26 +1,185 @@
 <template>
   <view class="pages-checkbox">
+    <demo-block title="基本用法">
+      <van-checkbox
+          v-model="checkbox1"
+          custom-class="demo-checkbox"
+      >
+        复选框
+      </van-checkbox>
+    </demo-block>
 
+    <demo-block title="禁用状态" disabled>
+      <van-checkbox
+          disabled
+          :value="false"
+          custom-class="demo-checkbox"
+      >
+        复选框
+      </van-checkbox>
+      <van-checkbox
+          disabled
+          :value="true"
+          custom-class="demo-checkbox"
+      >
+        复选框
+      </van-checkbox>
+    </demo-block>
+
+    <demo-block title="自定义形状">
+      <van-checkbox
+          v-model="checkboxShape"
+          shape="square"
+          custom-class="demo-checkbox"
+      >
+        复选框
+      </van-checkbox>
+    </demo-block>
+
+    <demo-block title="自定义颜色">
+      <van-checkbox
+          v-model="checkbox2"
+          checked-color="#07c160"
+          custom-class="demo-checkbox"
+      >
+        复选框
+      </van-checkbox>
+    </demo-block>
+
+    <demo-block title="自定义图标">
+      <van-checkbox
+          use-icon-slot
+          v-model="checkbox3"
+          custom-class="demo-checkbox"
+      >
+        自定义图标
+        <image
+            slot="icon"
+            class="icon"
+            mode="widthFix"
+            :src="checkbox3 ? activeIcon : inactiveIcon"
+        />
+      </van-checkbox>
+    </demo-block>
+
+    <demo-block title="复选框组">
+      <van-checkbox-group v-model="result">
+        <van-checkbox
+            v-for="(item, index) in list"
+            :key="index"
+            :name="item"
+            custom-class="demo-checkbox"
+        >
+          复选框 {{ item }}
+        </van-checkbox>
+      </van-checkbox-group>
+    </demo-block>
+
+    <demo-block title="禁用复选框组">
+      <van-checkbox-group v-model="result4" disabled>
+        <van-checkbox
+            v-for="(item, index) in list"
+            :key="index"
+            :name="item"
+            custom-class="demo-checkbox"
+        >
+          复选框 {{ item }}
+        </van-checkbox>
+      </van-checkbox-group>
+    </demo-block>
+
+    <demo-block title="设置最大可选数">
+      <van-checkbox-group :max="2" v-model="result2">
+        <van-checkbox
+            v-for="(item, index) in list"
+            :key="index"
+            :name="item"
+            custom-class="demo-checkbox"
+        >
+          复选框 {{ item }}
+        </van-checkbox>
+      </van-checkbox-group>
+    </demo-block>
+
+    <demo-block title="搭配单元格组件使用">
+      <van-checkbox-group v-model="result3">
+        <van-cell-group >
+          <van-cell
+              v-for="(item, index) in list"
+              :key="index"
+              :title="'复选框 ' + item"
+              value-class="value-class"
+              clickable
+              @tap="toggle(index)"
+          >
+            <van-checkbox @tap="noop" :ref="'page-checkbox-'+ index" :name="item" slot="right-icon" />
+          </van-cell>
+        </van-cell-group>
+      </van-checkbox-group>
+    </demo-block>
   </view>
 </template>
 
 <script>
   import DemoBlock from "../../../components/app/demo-block";
+  import VanCheckboxGroup from "../../../components/vant/checkbox-group/index";
+  import VanCheckbox from "../../../components/vant/checkbox/index";
+  import VanCellGroup from "../../../components/vant/cell-group/index";
+  import VanCell from "../../../components/vant/cell/index";
 
   export default {
     name: 'pages-checkbox',
-    onLoad(){
+    components: {VanCell, VanCellGroup, VanCheckbox, VanCheckboxGroup, DemoBlock},
 
+    data(){
+      return {
+        parentGroup: {},
+
+        checkbox1: false,
+        checkbox2: true,
+        checkbox3: true,
+        checkboxShape: true,
+        list: ['a', 'b', 'c'],
+        result: ['a', 'b'],
+        result2: [],
+        result3: [],
+        result4: ['a', 'b'],
+        activeIcon: 'https://img.yzcdn.cn/vant/user-active.png',
+        inactiveIcon: 'https://img.yzcdn.cn/vant/user-inactive.png'
+      }
+    },
+
+    onLoad(){
     },
     onUnload(){
-
     },
     methods: {
 
+      onChange(val) {
+        this.checkbox1 = val
+      },
+      toggle(index){
+        this.$refs[`page-checkbox-${index}`][0].toggle();
+      },
+      noop() {}
     }
   }
 </script>
 
 <style lang="less">
+  .demo-checkbox-group {
+    margin: 10px 0 0 20px;
+  }
 
+  .demo-checkbox {
+    margin: 10px 0 0 20px;
+  }
+
+  .value-class {
+    flex: none !important;
+  }
+
+  .icon {
+    width: 20px;
+  }
 </style>
