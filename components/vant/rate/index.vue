3c3465b4d388558485448dfb4cd0e74f9d1af233
wangy <wangyang@zjbdos.com>
2019-11-14 17:06:12 +0800

    rate 评分

--- components/vant/rate/index.vue
+++ components/vant/rate/index.vue
@@ -1,48 +1,53 @@
 <template>
 
-<view
-  class="van-rate custom-class"
-  @touchmove="onTouchMove"
->
   <view
-    class="van-rate__item"
-    v-for=" count "
-    :key="index"
-    style="font-size: {{ sizeWithUnit }};padding-right: {{ index !== count - 1 ? gutterWithUnit : '' }}"
+    class="van-rate custom-class"
+    @touchmove="onTouchMove"
   >
-    <van-icon
-      :name=" index + 1 <= innerValue ? icon : voidIcon "
-      class="van-rate__icon"
-      custom-class="icon-class"
-      :data-score=" index "
-      :color=" disabled ? disabledColor : index + 1 <= innerValue ? color : voidColor "
-      @click="onSelect"
-    />
+    <view
+      class="van-rate__item"
+      v-for=" (item,index) in count "
+      :key="index"
+      :style="'font-size:' + sizeWithUnit + ';padding-right:' + (index !== count - 1 ? gutterWithUnit : '') "
+    >
+      <van-icon
+        :name=" index + 1 <= innerValue ? icon : voidIcon "
+        class="van-rate__icon"
+        :custom-class="iconClass"
+        :data-score=" index "
+        :color=" disabled ? disabledColor : index + 1 <= innerValue ? color : voidColor "
+        @click="onSelect(index)"
+      />
 
-    <van-icon
-      v-if=" allowHalf "
-      :name=" index + 0.5 <= innerValue ? icon : voidIcon "
-      :class=" utils.bem('rate__icon', ['half']) "
-      custom-class="icon-class"
-      :data-score=" index - 0.5 "
-      :color=" disabled ? disabledColor : index + 0.5 <= innerValue ? color : voidColor "
-      @click="onSelect"
-    />
+      <van-icon
+        v-if=" allowHalf "
+        :name=" index + 0.5 <= innerValue ? icon : voidIcon "
+        :class="rateIcon"
+        :custom-class="iconClass"
+        :data-score=" index - 0.5 "
+        :color=" disabled ? disabledColor : index + 0.5 <= innerValue ? color : voidColor "
+        @click="onSelect"
+      />
+    </view>
   </view>
-</view>
 
 </template>
 
 <script>
-  import utils from '../wxs/utils';
+import utils from '../wxs/utils';
+import { basic } from '../mixins/basic';
 
-import { Weapp } from 'definitions/weapp';
+// import { Weapp } from 'definitions/weapp';
 import { addUnit } from '../common/utils';
+import VanIcon from "../icon/index"
+import { type } from 'os';
 
 export default {
+  name: "van-rate",
   field: true,
+  components: { VanIcon },
+  mixins: [basic],
 
-  classes: ['icon-class'],
 
   props: {
     value: Number,
@@ -51,95 +56,109 @@ export default {
     allowHalf: Boolean,
     size: {
       type: null,
-      observer: 'setSizeWithUnit'
     },
     icon: {
       type: String,
-      value: 'star'
+      default: 'star'
     },
     voidIcon: {
       type: String,
-      value: 'star-o'
+      default: 'star-o'
     },
     color: {
       type: String,
-      value: '#ffd21e'
+      default: '#ffd21e'
     },
     voidColor: {
       type: String,
-      value: '#c7c7c7'
+      default: '#c7c7c7'
     },
     disabledColor: {
       type: String,
-      value: '#bdbdbd'
+      default: '#bdbdbd'
     },
     count: {
       type: Number,
-      value: 5
+      default: 5
     },
     gutter: {
       type: null,
-      observer: 'setGutterWithUnit'
     },
     touchable: {
       type: Boolean,
-      value: true
+      default: true
+    },
+    iconClass: {
+      type: String,
+      default: ''
+    },
+  },
+
+  computed: {
+    rateIcon () {
+      return `${utils.bem('rate__icon', ['half'])}`
     }
   },
 
-  data: {
-    innerValue: 0,
-    gutterWithUnit: undefined,
-    sizeWithUnit: '20px'
+  data () {
+    return {
+      innerValue: 0,
+      gutterWithUnit: undefined,
+      sizeWithUnit: '20px'
+    }
+  },
+
+  beforeCreate () {
+    this.$nextTick(() => {
+      this.innerValue = this.value
+    })
   },
 
   watch: {
-    value(value: number) {
-      if (value !== this.data.innerValue) {
-        this.setData({ innerValue: value });
+    value (value) {
+      if (value !== this.innerValue) {
+        this.innerValue = value
       }
+    },
+    size () {
+      this.setSizeWithUnit()
+    },
+    gutter () {
+      this.setGutterWithUnit()
     }
   },
 
   methods: {
-    setSizeWithUnit(val) {
-      this.setData({
-        sizeWithUnit: addUnit(val)
-      });
+    setSizeWithUnit (val) {
+      this.sizeWithUnit = addUnit(val)
     },
 
-    setGutterWithUnit(val) {
-      this.setData({
-        gutterWithUnit: addUnit(val)
-      });
+    setGutterWithUnit (val) {
+      this.gutterWithUnit = addUnit(val)
     },
 
-    onSelect(event) {
-      const { data } = this;
-      const { score } = event.currentTarget.dataset;
-      if (!data.disabled && !data.readonly) {
-        this.setData({ innerValue: score + 1 });
-        this.$emit('input', score + 1);
-        this.$emit('change', score + 1);
+    onSelect (score) {
+      const scores = parseInt(score)
+      if (!this.disabled && !this.readonly) {
+        this.innerValue = scores + 1
+        this.$emit('input', scores + 1);
+        this.$emit('change', scores + 1);
       }
     },
 
-    onTouchMove(event: Weapp.TouchEvent) {
-      const { touchable } = this.data;
+    onTouchMove (event) {
+      const { touchable } = this;
       if (!touchable) return;
 
       const { clientX } = event.touches[0];
 
       this.getRect('.van-rate__icon', true).then(
-        (list: WechatMiniprogram.BoundingClientRectCallbackResult[]) => {
+        (list) => {
           const target = list
             .sort(item => item.right - item.left)
             .find(item => clientX >= item.left && clientX <= item.right);
           if (target != null) {
-            this.onSelect({
-              ...event,
-              currentTarget: target
-            });
+            this.onSelect(target.dataset.score);
           }
         }
       );
@@ -150,5 +169,4 @@ export default {
 </script>
 
 <style lang="less">
-
 </style>
--- pages/vant/rate/rate.vue
+++ pages/vant/rate/rate.vue
@@ -1,26 +1,124 @@
 <template>
   <view class="pages-rate">
+    <demo-block title="基础用法">
+      <van-rate
+        custom-class="van-rate"
+        data-key="value.value1"
+        :value="value.value1"
+      />
+    </demo-block>
 
+    <demo-block title="自定义图标">
+      <van-rate
+        custom-class="van-rate"
+        icon="like"
+        void-icon="like-o"
+        data-key="value.value2"
+        :value="value.value2"
+      />
+    </demo-block>
+
+    <demo-block title="自定义样式">
+      <van-rate
+        custom-class="van-rate"
+        data-key="value.value3"
+        :value="value.value3"
+        size="25"
+        color="#ee0a24"
+        void-color="#eee"
+        void-icon="star"
+        @change="onChange"
+      />
+    </demo-block>
+
+    <demo-block title="半星">
+      <van-rate
+        custom-class="van-rate"
+        data-key="value.value4"
+        :value="value.value4"
+        size="25"
+        allow-half
+        color="#ee0a24"
+        void-color="#eee"
+        void-icon="star"
+        :touchable="false"
+        @change="onChange"
+      />
+    </demo-block>
+
+    <demo-block title="自定义数量">
+      <van-rate
+        custom-class="van-rate"
+        data-key="value.value5"
+        :value="value.value5"
+        :count="6"
+        @change="onChange"
+      />
+    </demo-block>
+
+    <demo-block title="禁用状态">
+      <van-rate
+        custom-class="van-rate"
+        data-key="value.value6"
+        :value="value.value6"
+        disabled
+      />
+    </demo-block>
+
+    <demo-block title="只读状态">
+      <van-rate
+        custom-class="van-rate"
+        data-key="value.value6"
+        :value="value.value6"
+        readonly
+      />
+    </demo-block>
   </view>
 </template>
 
 <script>
-  import DemoBlock from "../../../components/app/demo-block";
+import DemoBlock from "../../../components/app/demo-block";
+import VanRate from "../../../components/vant/rate/index.vue"
 
-  export default {
-    name: 'pages-rate',
-    onLoad(){
+export default {
+  name: 'pages-rate',
+  components: { VanRate, DemoBlock },
+  data () {
+    return {
+      value: {
+        value1: 3,
+        value2: 3,
+        value3: 3,
+        value4: 2.5,
+        value5: 4,
+        value6: 3
+      }
+    }
+  },
+  onLoad () {
 
-    },
-    onUnload(){
+  },
+  onUnload () {
 
-    },
-    methods: {
-
+  },
+  methods: {
+    onChange (event) {
+      console.log(event)
+      // const { key } = event.currentTarget.dataset;
+      // this.setData({
+      //   [key]: event.detail
+      // });
     }
   }
+}
 </script>
 
 <style lang="less">
+page {
+  background-color: #fff;
+}
 
+.van-rate {
+  margin-left: 15px;
+}
 </style>
