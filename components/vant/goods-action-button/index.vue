f94a765a6249433b59e079417814f57f95961a64
wangy <wangyang@zjbdos.com>
2019-11-18 10:31:53 +0800

    goods-action 商品导航

--- components/vant/goods-action-button/index.vue
+++ components/vant/goods-action-button/index.vue
@@ -1,51 +1,55 @@
 <template>
 
-<van-button
-  square
-  :id=" id "
-  :lang=" lang "
-  :type=" type "
-  :color=" color "
-  :loading=" loading "
-  :disabled=" disabled "
-  :open-type=" openType "
-  custom-class="{{ utils.bem('goods-action-button', [type, { first: isFirst, last: isLast }]) }}"
-  :business-id=" businessId "
-  :session-from=" sessionFrom "
-  :app-parameter=" appParameter "
-  :send-message-img=" sendMessageImg "
-  :send-message-path=" sendMessagePath "
-  :show-message-card=" showMessageCard "
-  :send-message-title=" sendMessageTitle "
-  @click="onClick"
-  binderror="bindError"
-  bindcontact="bindContact"
-  bindopensetting="bindOpenSetting"
-  bindgetuserinfo="bindGetUserInfo"
-  bindgetphonenumber="bindGetPhoneNumber"
-  bindlaunchapp="bindLaunchApp"
->
-  {{ text }}
-</van-button>
+  <van-button
+    square
+    :id=" id "
+    :lang=" lang "
+    :type=" type "
+    :color=" color "
+    :loading=" loading "
+    :disabled=" disabled "
+    :open-type=" openType "
+    :custom-class="goodactionButton"
+    :business-id=" businessId "
+    :session-from=" sessionFrom "
+    :app-parameter=" appParameter "
+    :send-message-img=" sendMessageImg "
+    :send-message-path=" sendMessagePath "
+    :show-message-card=" showMessageCard "
+    :send-message-title=" sendMessageTitle "
+    @click="onClick"
+    @error="bindError"
+    @contact="bindContact"
+    @opensetting="bindOpenSetting"
+    @getuserinfo="bindGetUserInfo"
+    @getphonenumber="bindGetPhoneNumber"
+    @launchapp="bindLaunchApp"
+  >
+    {{ text }}
+  </van-button>
 
 </template>
 
 <script>
-  import utils from '../wxs/utils';
+import utils from '../wxs/utils';
 
 import { link } from '../mixins/link';
 import { button } from '../mixins/button';
 import { openType } from '../mixins/open-type';
-import { Weapp } from 'definitions/weapp';
+import { queryParentComponent } from "../common/utils";
+// import { Weapp } from 'definitions/weapp';
+import VanButton from "../button/index"
 
 export default {
+  name: "van-goods-action-button",
+  components: { VanButton },
   mixins: [link, button, openType],
   relation: {
     type: 'ancestor',
     name: 'goods-action',
-    linked(parent) {
-      this.parent = parent;
-    }
+    // linked(parent) {
+    //   this.parent = parent;
+    // }
   },
   props: {
     text: String,
@@ -54,28 +58,61 @@ export default {
     disabled: Boolean,
     type: {
       type: String,
-      value: 'danger'
+      default: 'danger'
     }
   },
 
-  mounted() {
-    this.updateStyle();
+  data () {
+    return {
+      hasParent: false,
+      isFirst: false,
+      isLast: false
+    }
   },
 
+  mounted () {
+    this.parent = queryParentComponent(this, 'van-goods-action');
+    if (this.parent) {
+      this.hasParent = true;
+      this.parent.linked(this);
+      this.updateStyle();
+    }
+  },
+
+  destroyed () {
+    if (this.parent) {
+      this.parent.unlinked(this);
+      this.parent = null;
+      this.hasParent = false;
+    }
+  },
+
+  computed: {
+    goodactionButton () {
+      // console.log(`${utils.bem('goods-action-button', [this.type, { first: this.isFirst, last: this.isLast }])}`)
+      return `${utils.bem('goods-action-button', [this.type, { first: this.isFirst, last: this.isLast }])}`
+    }
+  },
+
+
   methods: {
-    onClick(event) {
-      this.$emit('click', event.detail);
+    onClick (event) {
+      this.$emit('click');
       this.jumpLink();
     },
 
-    updateStyle() {
+    updateStyle () {
       const { children = [] } = this.parent;
       const index = children.indexOf(this);
-
-      this.setData({
-        isFirst: index === 0,
-        isLast: index === children.length - 1
-      });
+      this.isFirst = index === 0
+      // this.isLast = index === children.length - 1
+      // console.log(this.isFirst, this.isLast)
+      if (index == 0) {
+        console.log(index)
+        this.isLast = false
+      } else {
+        this.isLast = true
+      }
     }
   }
 };
@@ -83,5 +120,4 @@ export default {
 </script>
 
 <style lang="less">
-
 </style>
--- components/vant/goods-action-icon/index.vue
+++ components/vant/goods-action-icon/index.vue
@@ -1,42 +1,42 @@
 <template>
-    <van-button
-  square
-  :id=" id "
-  size="large"
-  :lang=" lang "
-  :loading=" loading "
-  :disabled=" disabled "
-  :open-type=" openType "
-  :business-id=" businessId "
-  custom-class="van-goods-action-icon"
-  :session-from=" sessionFrom "
-  :app-parameter=" appParameter "
-  :send-message-img=" sendMessageImg "
-  :send-message-path=" sendMessagePath "
-  :show-message-card=" showMessageCard "
-  :send-message-title=" sendMessageTitle "
-  @click="onClick"
-  binderror="bindError"
-  bindcontact="bindContact"
-  bindopensetting="bindOpenSetting"
-  bindgetuserinfo="bindGetUserInfo"
-  bindgetphonenumber="bindGetPhoneNumber"
-  bindlaunchapp="bindLaunchApp"
->
-  <view class="van-goods-action-icon__content">
-    <van-icon
-      v-if=" icon "
-      size="20px"
-      :name=" icon "
-      :dot=" dot "
-      :info=" info "
-      class="van-goods-action-icon__icon"
-      custom-class="icon-class"
-    />
-    <slot name="icon" />
-    <text class="text-class">{{ text }}</text>
-  </view>
-</van-button>
+  <van-button
+    square
+    :id=" id "
+    size="large"
+    :lang=" lang "
+    :loading=" loading "
+    :disabled=" disabled "
+    :open-type=" openType "
+    :business-id=" businessId "
+    custom-class="van-goods-action-icon"
+    :session-from=" sessionFrom "
+    :app-parameter=" appParameter "
+    :send-message-img=" sendMessageImg "
+    :send-message-path=" sendMessagePath "
+    :show-message-card=" showMessageCard "
+    :send-message-title=" sendMessageTitle "
+    @click="onClick"
+    @error="bindError"
+    @contact="bindContact"
+    @opensetting="bindOpenSetting"
+    @getuserinfo="bindGetUserInfo"
+    @getphonenumber="bindGetPhoneNumber"
+    @launchapp="bindLaunchApp"
+  >
+    <view class="van-goods-action-icon__content">
+      <van-icon
+        v-if=" icon "
+        size="20px"
+        :name=" icon "
+        :dot=" dot "
+        :info=" info "
+        class="van-goods-action-icon__icon"
+        :custom-class="iconClass"
+      />
+      <slot name="icon" />
+      <text :class="textClass">{{ text }}</text>
+    </view>
+  </van-button>
 
 </template>
 
@@ -45,25 +45,37 @@
 import { link } from '../mixins/link';
 import { button } from '../mixins/button';
 import { openType } from '../mixins/open-type';
-import { Weapp } from 'definitions/weapp';
+// import { Weapp } from 'definitions/weapp';
+import { type } from 'os';
+import VanIcon from "../icon/index"
+import VanButton from "../button/index"
 
 export default {
-  classes: ['icon-class', 'text-class'],
+  name: "van-goods-action-icon",
+  // classes: ['icon-class', 'text-class'],
 
   mixins: [link, button, openType],
-
+  components: { VanIcon, VanButton },
   props: {
     text: String,
     dot: Boolean,
     info: String,
     icon: String,
     disabled: Boolean,
-    loading: Boolean
+    loading: Boolean,
+    iconClass: {
+      type: String,
+      default: ""
+    },
+    textClass: {
+      type: String,
+      default: ""
+    },
   },
 
   methods: {
-    onClick(event) {
-      this.$emit('click', event.detail);
+    onClick (event) {
+      this.$emit('click');
       this.jumpLink();
     }
   }
@@ -72,5 +84,4 @@ export default {
 </script>
 
 <style lang="less">
-
 </style>
