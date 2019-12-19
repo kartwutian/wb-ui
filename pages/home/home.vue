30a8f17b55629ff4494721e31300d66e04d32c89
bhz <baohz@zjbdos.com>
2019-12-19 17:42:46 +0800

    wb-uni-pro 模板 init

---
+++ pages/home/home.vue
@@ -0,0 +1,26 @@
+<template>
+  <view class="pages-home">
+    <view class="pages-home__header">hello world</view>
+  </view>
+</template>
+
+<script>
+  export default {
+    name: 'pages-home',
+    onLoad(){
+
+    },
+    onUnload(){
+
+    },
+    methods: {
+
+    }
+  }
+</script>
+
+<style lang="less">
+  .pages-home{
+
+  }
+</style>
